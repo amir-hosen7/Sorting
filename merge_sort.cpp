056c650e2acd5353470bfdc87200057005f95fe5
Amir Hosen <42311923+amir-hosen7@users.noreply.github.com>
2023-03-01 10:05:51 +0600

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,51 +1,31 @@
-#include<iostream>
-using namespace std;
-
-void partition(int L, int R, int arr[]) {
-    if (L == R) return;
-    int mid = L + (R - L) / 2;
-    //dividing
-    partition(L, mid, arr);
-    partition(mid + 1, R, arr);
-    
-    // merging
-    int l = mid - L + 1, r = R - mid;
-    int left[l + 5], right[r + 5];
-    for (int i = 0; i < l; i++) {
-        left[i] = arr[L + i];
+class Solution {
+public:
+    void combine(int low, int mid, int high, vector<int>& v) {
+        if (low >= high) return;
+        int L = low, R = mid + 1, siz = high - low + 1, idx = 0;
+        vector<int> sorted(siz, 0);
+        while (L <= mid and R <= high) {
+            if (v[L] < v[R]) sorted[idx++] = v[L++];
+            else sorted[idx++] = v[R++];
+        }
+        while (L <= mid) sorted[idx++] = v[L++];
+        while (R <= high) sorted[idx++] = v[R++];
+        for (int i = 0; i < siz; i++) {
+            v[low + i] = sorted[i];
+        }
     }
-    for (int i = 0; i < r; i++) {
-        right[i] = arr[mid + i + 1];
-    }
-    int idxL = 0, idxR = 0;
-    while (idxL < l and idxR < r) {
-        if (left[idxL] <= right[idxR]) arr[L++] = left[idxL++];
-        else arr[L++] = right[idxR++];
-    }
-    while (idxL < l) arr[L++] = left[idxL++];
-    while (idxR < r) arr[L++] = right[idxR++];
-}
 
-void merge_sort(int n, int arr[]) {
-    partition(0, n - 1, arr);
-}
-
-void print(int n, int arr[]) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    void partition(int L, int R, vector<int>& v) {
+        if (L < R) {
+            int mid = L + (R - L) / 2;
+            partition(L, mid, v);
+            partition(mid + 1, R, v);
+            combine(L, mid, R, v);
+        }
     }
-    cout << "\n";
-}
 
-int main() {
-    int n;
-    cin >> n;
-    int arr[n + 5];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    vector<int> sortArray(vector<int>& nums) {
+        partition(0, nums.size() - 1, nums);
+        return nums;
     }
-    merge_sort(n, arr);
-    print(n, arr);
-    return 0;
-}
-
+};
